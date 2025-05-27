75a1cd32437b75da738aa3104698f5971ed26346
Lorenzooka <75865917+Lorenzooka@users.noreply.github.com>
2025-05-27 02:05:30 +0200

    Add simple NFT showcase website

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for NFT interactions
+// You can add functionality such as fetching NFT metadata from a blockchain API
+// or adding animations.
+
+console.log('NFT Showcase loaded');
